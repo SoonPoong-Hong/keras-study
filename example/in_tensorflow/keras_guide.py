5d97cf274b4940ba6de2bdad2c6a707a067ba1cc
rocklike <rocklike@gmail.com>
2019-03-24 18:06:23 +0900

    keras test

---
+++ example/in_tensorflow/keras_guide.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+https://www.tensorflow.org/guide/keras
+에 있는 소스임.
+'''
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+print(tf.VERSION)
+print(tf.keras.__version__)
+
