38c5c666468f3918346db69334d5ce39976c7d53
rocklike <rocklike@gmail.com>
2019-04-03 00:48:50 +0900

    .

--- example/hong_test/kerasMnist.py
+++ example/hong_test/kerasMnist.py
@@ -14,3 +14,26 @@ np.random.seed(7)
 print('Python version : ', sys.version)
 print('TensorFlow version : ', tf.__version__)
 print('Keras version : ', keras.__version__)
+
+img_rows = 28
+img_cols = 28
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+input_shape = (img_rows, img_cols, 1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
